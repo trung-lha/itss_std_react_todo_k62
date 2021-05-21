3dfdfa348a954b5499f563cddf69e93e670cd613
trung-lha <62026316+trung-lha@users.noreply.github.com>
2021-05-21 22:25:04 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAltenAKnf2hK17TLANIlsSL1djcypTKok",
+    authDomain: "itss-2020-todos.firebaseapp.com",
+    projectId: "itss-2020-todos",
+    storageBucket: "itss-2020-todos.appspot.com",
+    messagingSenderId: "298557407633",
+    appId: "1:298557407633:web:ce041e38bf97deb24a9a30"
+  };
+
+  firebase.initializeApp(firebaseConfig);
