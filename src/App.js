29031629f53c57465bfabb83612c9786f8b9bbf0
trung-lha <62026316+trung-lha@users.noreply.github.com>
2021-05-22 01:41:45 +0700

    Login by Authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react';
+import Login from "./components/Login";
+import { auth, storeUserInfo, updateUser } from "./lib/firebase";
 
 /* スタイルシート */
 import './styles/main.css';
@@ -7,10 +9,56 @@ import './styles/main.css';
 import Todo from './components/Todo';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      setLoading(false);
+      let newUser = null;
+      if (user) {
+        newUser = await storeUserInfo(user);
+      }
+      setUser(newUser);
+    });
+  }, []);
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+
+  const HeaderContent = () => {
+    if (user) {
+      return (
+        <div class="navbar-end">
+          <div class="navbar-item">
+            {user.name}
+          </div>
+          <div class="navbar-item">
+            <button class="button is-danger is-light is-small" onClick={logout} > Logout</button>
+          </div>
+        </div >
+      )
+    } else {
+      return (<Login />)
+    }
+  }
   return (
     <div className="container is-fluid">
-      <Todo />
-    </div>
+      <header class="navbar">
+        {loading ? (
+          <p>
+            LOADING.....
+          </p>
+        ) : (
+          <HeaderContent />
+        )}
+      </header >
+      <div>
+        {user && <Todo />}
+      </div>
+    </div >
   );
 }
 
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -52,3 +52,40 @@ export const clearFirebaseItem = async (item) => {
     console.log(err);
   });
 }; 
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data() });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+export const auth = firebase.auth();
